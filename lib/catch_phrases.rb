ad6c98cb01469d086a0423a4374bd73a6da299c4
George Blackmore <gtblackmore@gmail.com>
2019-12-07 23:30:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,16 +4,7 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
-def link
-  phrase = "It's too Dangerous To Go Alone! Take This."
-  puts phrase
-end
-
-def any_phrase
-  phrase = "Do A Barrel Roll!"
-  puts phrase
-end
